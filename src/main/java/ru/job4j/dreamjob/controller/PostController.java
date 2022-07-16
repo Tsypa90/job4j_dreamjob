34d95b743e828af24da4b28e5ca7558d05479d73
Tsypa90 <URSA2004@mail.ru>
2022-07-16 22:59:26 +0300

    2. Формы. Списки. [#504854]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -3,37 +3,40 @@ package ru.job4j.dreamjob.controller;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.service.CityService;
 import ru.job4j.dreamjob.service.PostService;
 
 @Controller
 @ThreadSafe
 public class PostController {
     private final PostService service;
+    private final CityService cityService;
 
-    public PostController(PostService service) {
+    public PostController(PostService service, CityService cityService) {
         this.service = service;
+        this.cityService = cityService;
     }
 
 
     @GetMapping("/posts")
     public String posts(Model model) {
         model.addAttribute("posts", service.findAll());
+        model.addAttribute("cities", cityService.getAllCities());
         return "posts";
     }
 
     @GetMapping("/addPost")
     public String addPost(Model model) {
+        model.addAttribute("cities", cityService.getAllCities());
         return "addPost";
     }
     @GetMapping("/updatePost/{postId}")
     public String updatePost(Model model, @PathVariable("postId") int id) {
         model.addAttribute("post", service.findById(id));
+        model.addAttribute("cities", cityService.getAllCities());
         return "updatePost";
     }
     @PostMapping("/updatePost")
--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -14,6 +14,8 @@ public class Post implements Serializable {
 
     private boolean visible;
 
+    private City city;
+
     public Post() {
     }
 
@@ -54,6 +56,14 @@ public class Post implements Serializable {
 
     public void setCreated(LocalDate created) {
         this.created = created;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
     }
 
     public boolean isVisible() {
